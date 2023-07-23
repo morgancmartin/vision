4a53e7ae1e679fb23fdfebfe5dc7dcb49c2d1449
Morgan Martin <morganm5201@gmail.com>
2023-07-23 04:10:10 -0400

    new frontend

--- vision-frontend/app/routes/_index.tsx
+++ vision-frontend/app/routes/_index.tsx
@@ -1,66 +1,41 @@
-import type { ActionArgs } from "@remix-run/node";
-import { json, redirect } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
-import { useEffect, useRef } from "react";
+import type { V2_MetaFunction } from "@remix-run/node";
 
-import { createGenImage } from "~/models/genimage.server";
-import { requireUserId } from "~/session.server";
-import Homepage from "~/components/Homepage";
-
-export const action = async ({ request }: ActionArgs) => {
-  const userId = await requireUserId(request);
-
-  const formData = await request.formData();
-  const prompt = formData.get("prompt");
-
-  if (typeof prompt !== "string" || prompt.length === 0) {
-    return json(
-      { errors: { prompt: "Prompt is required", title: null } },
-      { status: 400 }
-    );
-  }
-
-  const image = await createGenImage({ prompt, userId });
-
-  return redirect(`/results`);
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
 };
 
 export default function Index() {
-  const actionData = useActionData<typeof action>();
-  const promptRef = useRef<HTMLTextAreaElement>(null);
-  const submitRef = useRef<HTMLButtonElement>(null);
-
-  useEffect(() => {
-    if (actionData?.errors?.prompt) {
-      promptRef.current?.focus();
-    }
-  }, [actionData]);
-
-  const onSubmit = () => submitRef.current?.click();
-
   return (
-    <Homepage className="overflow-hidden" onSubmit={onSubmit}>
-      <div className="h-24 w-full">
-        <Form method="post" className="w-full h-full">
-          <textarea
-            ref={promptRef}
-            name="prompt"
-            rows={4}
-            className="w-full flex-1 rounded-md text-lg leading-6 outline-0 border px-4 py-1"
-            aria-invalid={actionData?.errors?.prompt ? true : undefined}
-            aria-errormessage={
-              actionData?.errors?.prompt ? "prompt-error" : undefined
-            }
-          />
-          {actionData?.errors?.prompt ? (
-            <div className="text-red-700" id="prompt-error">
-              {actionData.errors.prompt}
-            </div>
-          ) : null}
-
-          <button ref={submitRef} type="submit" className="hidden" />
-        </Form>
-      </div>
-    </Homepage>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
--- vision-frontend/remix.config.js
+++ vision-frontend/remix.config.js
@@ -1,15 +1,17 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  server: "./server.js",
-  cacheDirectory: "./node_modules/.cache/remix",
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+  serverModuleFormat: "cjs",
   future: {
+    v2_dev: true,
     v2_errorBoundary: true,
+    v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
-  ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
-  postcss: true,
-  serverModuleFormat: "cjs",
-  tailwind: true,
 };
