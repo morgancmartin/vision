8445f24c1bb35d6c670e81e6dd1f6d938ab4a1dd
Morgan Martin <morganm5201@gmail.com>
2023-07-23 04:32:45 -0400

    update old frontend

--- old-vision-frontend/app/root.tsx
+++ old-vision-frontend/app/root.tsx
@@ -1,6 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction, LoaderArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -10,28 +9,20 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import { getUser } from "~/session.server";
-import stylesheet from "~/tailwind.css";
-
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
-export const loader = async ({ request }: LoaderArgs) => {
-  return json({ user: await getUser(request) });
-};
-
 export default function App() {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body className="h-full">
+      <body>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- vision-frontend/remix.config.js
+++ vision-frontend/remix.config.js
@@ -1,17 +1,15 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  serverModuleFormat: "esm",
+module.exports = {
+  cacheDirectory: "./node_modules/.cache/remix",
   future: {
-    v2_dev: true,
     v2_errorBoundary: true,
-    v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
+  ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
+  serverModuleFormat: "esm",
+  postcss: true,
+  serverModuleFormat: "cjs",
+  tailwind: true,
 };
